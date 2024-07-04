b1a07f6f3c8170db254a62661c51c5deb78ab6c5
Maulana Rangga Arrosyid <134927221+rangga13092004@users.noreply.github.com>
2024-07-04 09:10:50 +0700

    Add files via upload

---
+++ Tugas CIE406 - Keamanan Informasi/Tugas Sesi 10/Materi yang dibahas/Ketentuan Makalah.md
@@ -0,0 +1,6 @@
+Ketentuan Makalah Tugas Sesi 10 CIE406 - Keamaman Informasi:
+1. Cover
+2. Daftar Isi
+3. Isi
+4. Kesimpulan
+5. Referensi
---
+++ Tugas CIE406 - Keamanan Informasi/Tugas Sesi 10/Materi yang dibahas/Materi.rb
@@ -0,0 +1,10 @@
+MATERI YANG DIBAHAS SESI 10:
+1. Tripwire
+2. Program Crack
+3. WinNuke
+4. Nmap
+5. Strobe
+6. TcpProbe
+7. Netlab
+8. CyberKit
+9. Proram Ballista
